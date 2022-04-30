f41a6a8da2ddf68c8756723f4e463412009b2984
ibrt2016 <ibrt2012@gmail.com>
2022-04-30 13:45:56 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "azurerm_resource_group" "rg" {
 
 #Create virtual network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-webserver-${azurerm_resource_group.rg.location}-001"
+  name                = "vnet-webserver-${azurerm_resource_group.rg.location}-002"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -34,7 +34,7 @@ resource "azurerm_virtual_network" "vnet" {
 
 # Create subnet
 resource "azurerm_subnet" "subnet" {
-  name                 = "snet-webserver-${azurerm_resource_group.rg.location}-001"
+  name                 = "snet-webserver-${azurerm_resource_group.rg.location}-002"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.0.0/24"]
